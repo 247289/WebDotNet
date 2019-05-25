3c5e0e862b13591c44d2163dda26174f1b9111e2
thangoc <thangoc247@gmail.com>
2019-05-26 01:15:11 +0700

    update
    update admin gui

--- Shop_Mobi/Areas/Admin/Controllers/HomeController.cs
+++ Shop_Mobi/Areas/Admin/Controllers/HomeController.cs
@@ -13,21 +13,17 @@ namespace Shop_Mobi.Areas.Admin.Controllers
         // GET: Admin/Home
         public ActionResult Index()
         {
-            Shop_Mobi.Databases.tbl_taikhoan acc = null;
+            //Shop_Mobi.Databases.tbl_taikhoan acc = null;
             if (Session["UsersAdmin"] != null)
             {
-                acc = (Shop_Mobi.Databases.tbl_taikhoan)Session["UsersAdmin"];
-                ViewBag.Account = acc;
-                //
-                Session["acc"] = acc;
+                //acc = (Shop_Mobi.Databases.tbl_taikhoan)Session["UsersAdmin"];
+                //Session["acc"] = acc;
                 return View();
             }
             else if (Session["UsersEmployee"] != null)
             {
-                acc = (Shop_Mobi.Databases.tbl_taikhoan)Session["UsersEmployee"];
-                ViewBag.Account = acc;
-                //
-                Session["acc"] = acc;
+                //acc = (Shop_Mobi.Databases.tbl_taikhoan)Session["UsersEmployee"];
+                //Session["acc"] = acc;
                 return View();
             }
             return Redirect("~/Admin/Account/Login");
@@ -44,27 +40,11 @@ namespace Shop_Mobi.Areas.Admin.Controllers
             var list_chitietdonhang = db.tbl_chitietdonhang.ToList();
             return View(list_chitietdonhang);
         }
-        public ActionResult danhmuc()
-        {
-            var list_danhmuc = db.tbl_danhmuc.ToList();
-            return View(list_danhmuc);
-        }
-        public ActionResult nhasanxuat()
-        {
-            var list_nhasanxuat = db.tbl_nhasanxuat.ToList();
-            return View(list_nhasanxuat);
-        }
 
         public ActionResult taikhoan()
         {
             var list_taikhoan = db.tbl_taikhoan.ToList();
             return View(list_taikhoan);
-        }
-
-        public ActionResult thongtincanhan()
-        {
-            var list_thongtincanhan = db.tbl_thongtincanhan.ToList();
-            return View(list_thongtincanhan);
         }
 
 
